e7b446454450ace8b2a251ecf845ae95b3040480
Josephat-n <josphatnjoroge254@gmail.com>
2019-10-29 03:57:43 +0300

    enable input property binding of a child component from the paent component.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,7 +9,7 @@ import {Quote} from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
+    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date()),
     new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
     new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
    
