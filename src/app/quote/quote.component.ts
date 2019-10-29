ee011f50bc9582c62c7b2c4c3b54e24dd1ca2494
Josephat-n <josphatnjoroge254@gmail.com>
2019-10-29 05:41:36 +0300

    add a function to delete a quote.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -17,7 +17,16 @@ export class QuoteComponent implements OnInit {
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
+ 
+  deleteQuote(isComplete, index){
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
 
+      if (toDelete){
+        this.quotes.splice(index,1)
+      }
+    }
+  }
   constructor() { }
 
   ngOnInit() {
