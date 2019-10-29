a7de866ddbe4f310e841961a4be5cbd2b4e60819
Josephat-n <josphatnjoroge254@gmail.com>
2019-10-29 15:03:39 +0300

    add form validation method

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   public showDescription: boolean;
-  constructor(public id: number,public name: string, public description: string, public author: string, public createDate: Date){
+  constructor(public id: number,public name: string, public author: string, public votes: number, public createDate: Date){
     this.showDescription=false;
   }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,9 +9,8 @@ import {Quote} from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', 'Njoro', new Date()),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot','Josphat',new Date(2019,6,9)),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon', 'TechWiz', new Date(2022,1,12)),
+    new Quote(1, 'A day spent without struggling is a wasted day.','Njoro', 4, new Date()),
+    new Quote(2,'The energy you spend borrowing a tool should be the same energy spent while returning it.','Josphat', 2, new Date(2019,6,9)),
    
   ];
   toggleDetails(index){
@@ -26,6 +25,13 @@ export class QuoteComponent implements OnInit {
         this.quotes.splice(index,1)
       }
     }
+  }
+
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
   }
   constructor() { }
 
