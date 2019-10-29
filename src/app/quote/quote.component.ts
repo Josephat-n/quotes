280185f9648e6890e7599df4f2dfc88846437bcb
Josephat-n <josphatnjoroge254@gmail.com>
2019-10-29 04:24:14 +0300

    add an author property in the Quote class defination.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,9 +9,9 @@ import {Quote} from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date()),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
+    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', 'Njoro', new Date()),
+    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot','Josphat',new Date(2019,6,9)),
+    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon', 'TechWiz', new Date(2022,1,12)),
    
   ];
 
